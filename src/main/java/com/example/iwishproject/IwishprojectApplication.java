85b8a9837b9cc878a0ff7e02e2af4f5c32cf59cc
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-04-26 12:50:51 +0200

    Initial commit

---
+++ src/main/java/com/example/iwishproject/IwishprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.iwishproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwishprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IwishprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iwishproject/IwishprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iwishproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwishprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
